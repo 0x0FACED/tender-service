3bfc32910df324b60b9b8b5ae1e384b914505f83
0x0FACED <https.genius@gmail.com>
2024-09-09 16:53:48 +0300

    refactor: echo.Context -> context.Context in methods args

--- internal/app/services_impl/bid_service_impl.go
+++ internal/app/services_impl/bid_service_impl.go
@@ -1,10 +1,11 @@
 package servicesimpl
 
 import (
+	"context"
+
 	"github.com/0x0FACED/tender-service/internal/app/database"
 	"github.com/0x0FACED/tender-service/internal/app/domain/models"
 	"github.com/0x0FACED/tender-service/internal/app/domain/repos"
-	"github.com/labstack/echo/v4"
 )
 
 // Всякие валидации здесь будут и вызовы БД
@@ -18,42 +19,42 @@ func NewBidService(db database.BidRepository) repos.BidService {
 	}
 }
 
-func (b *BidServiceImpl) GetUserBids(ctx echo.Context, params repos.GetUserBidsParams) error {
+func (b *BidServiceImpl) GetUserBids(ctx context.Context, params repos.GetUserBidsParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) CreateBid(ctx echo.Context, params repos.CreateBidParams) error {
+func (b *BidServiceImpl) CreateBid(ctx context.Context, params repos.CreateBidParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) EditBid(ctx echo.Context, bidId repos.BidId, params repos.EditBidParams) (models.Bid, error) {
+func (b *BidServiceImpl) EditBid(ctx context.Context, bidId repos.BidId, username repos.Username, params repos.EditBidParams) (models.Bid, error) {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) SubmitBidFeedback(ctx echo.Context, bidId repos.BidId, params repos.SubmitBidFeedbackParams) error {
+func (b *BidServiceImpl) SubmitBidFeedback(ctx context.Context, bidId repos.BidId, params repos.SubmitBidFeedbackParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) RollbackBid(ctx echo.Context, bidId repos.BidId, version int32, params repos.RollbackBidParams) error {
+func (b *BidServiceImpl) RollbackBid(ctx context.Context, bidId repos.BidId, version int32, params repos.RollbackBidParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) GetBidStatus(ctx echo.Context, bidId repos.BidId, params repos.GetBidStatusParams) error {
+func (b *BidServiceImpl) GetBidStatus(ctx context.Context, bidId repos.BidId, params repos.GetBidStatusParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) UpdateBidStatus(ctx echo.Context, bidId repos.BidId, params repos.UpdateBidStatusParams) error {
+func (b *BidServiceImpl) UpdateBidStatus(ctx context.Context, bidId repos.BidId, params repos.UpdateBidStatusParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) SubmitBidDecision(ctx echo.Context, bidId repos.BidId, params repos.SubmitBidDecisionParams) error {
+func (b *BidServiceImpl) SubmitBidDecision(ctx context.Context, bidId repos.BidId, params repos.SubmitBidDecisionParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) GetBidsForTender(ctx echo.Context, tenderId repos.TenderId, params repos.GetBidsForTenderParams) error {
+func (b *BidServiceImpl) GetBidsForTender(ctx context.Context, tenderId repos.TenderId, params repos.GetBidsForTenderParams) error {
 	panic("impl me")
 }
 
-func (b *BidServiceImpl) GetBidReviews(ctx echo.Context, tenderId repos.TenderId, params repos.GetBidReviewsParams) error {
+func (b *BidServiceImpl) GetBidReviews(ctx context.Context, tenderId repos.TenderId, params repos.GetBidReviewsParams) error {
 	panic("impl me")
 }
