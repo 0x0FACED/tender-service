f09577e4ed5ee5e5a9be3804a48128ea24a90340
0x0FACED <https.genius@gmail.com>
2024-09-09 16:54:33 +0300

    refactor: echo.Context -> context.Context in methods args

--- internal/app/services_impl/tender_service_impl.go
+++ internal/app/services_impl/tender_service_impl.go
@@ -1,9 +1,10 @@
 package servicesimpl
 
 import (
+	"context"
+
 	"github.com/0x0FACED/tender-service/internal/app/database"
 	"github.com/0x0FACED/tender-service/internal/app/domain/repos"
-	"github.com/labstack/echo/v4"
 )
 
 type TenderServiceImpl struct {
@@ -17,46 +18,46 @@ func NewTenderService(db database.TenderRepository) repos.TenderService {
 }
 
 // Получение списка предложений для тендера
-func (t *TenderServiceImpl) GetBidsForTender(ctx echo.Context, tenderId repos.TenderId, params repos.GetBidsForTenderParams) error {
+func (t *TenderServiceImpl) GetBidsForTender(ctx context.Context, tenderId repos.TenderId, params repos.GetBidsForTenderParams) error {
 	panic("impl me")
 }
 
 // Просмотр отзывов на прошлые предложения
-func (t *TenderServiceImpl) GetBidReviews(ctx echo.Context, tenderId repos.TenderId, params repos.GetBidReviewsParams) error {
+func (t *TenderServiceImpl) GetBidReviews(ctx context.Context, tenderId repos.TenderId, params repos.GetBidReviewsParams) error {
 	panic("impl me")
 }
 
 // Получение списка тендеров
-func (t *TenderServiceImpl) GetTenders(ctx echo.Context, params repos.GetTendersParams) error {
+func (t *TenderServiceImpl) GetTenders(ctx context.Context, params repos.GetTendersParams) error {
 	panic("impl me")
 }
 
 // Получение тендеров пользователя
-func (t *TenderServiceImpl) GetUserTenders(ctx echo.Context, params repos.GetUserTendersParams) error {
+func (t *TenderServiceImpl) GetUserTenders(ctx context.Context, params repos.GetUserTendersParams) error {
 	panic("impl me")
 }
 
 // Создание нового тендера
-func (t *TenderServiceImpl) CreateTender(ctx echo.Context, params repos.CreateTenderParams) error {
+func (t *TenderServiceImpl) CreateTender(ctx context.Context, params repos.CreateTenderParams) error {
 	panic("impl me")
 }
 
 // Редактирование тендера
-func (t *TenderServiceImpl) EditTender(ctx echo.Context, tenderId repos.TenderId, params repos.EditTenderParams) error {
+func (t *TenderServiceImpl) EditTender(ctx context.Context, tenderId repos.TenderId, username repos.Username, params repos.EditTenderParams) error {
 	panic("impl me")
 }
 
 // Откат версии тендера
-func (t *TenderServiceImpl) RollbackTender(ctx echo.Context, tenderId repos.TenderId, version int32, params repos.RollbackTenderParams) error {
+func (t *TenderServiceImpl) RollbackTender(ctx context.Context, tenderId repos.TenderId, version int32, params repos.RollbackTenderParams) error {
 	panic("impl me")
 }
 
 // Получение текущего статуса тендера
-func (t *TenderServiceImpl) GetTenderStatus(ctx echo.Context, tenderId repos.TenderId, params repos.GetTenderStatusParams) error {
+func (t *TenderServiceImpl) GetTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.GetTenderStatusParams) error {
 	panic("impl me")
 }
 
 // Изменение статуса тендера
-func (t *TenderServiceImpl) UpdateTenderStatus(ctx echo.Context, tenderId repos.TenderId, params repos.UpdateTenderStatusParams) error {
+func (t *TenderServiceImpl) UpdateTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.UpdateTenderStatusParams) error {
 	panic("impl me")
 }
