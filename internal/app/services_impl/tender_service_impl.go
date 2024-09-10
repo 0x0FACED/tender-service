7af58faf6a9992f7351f2ca8d3dbcdd0acc91e7b
0x0FACED <https.genius@gmail.com>
2024-09-10 17:35:20 +0300

    refactor: removed 2 methods, changed return vals

--- internal/app/services_impl/tender_service_impl.go
+++ internal/app/services_impl/tender_service_impl.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/0x0FACED/tender-service/internal/app/database"
+	"github.com/0x0FACED/tender-service/internal/app/domain/models"
 	"github.com/0x0FACED/tender-service/internal/app/domain/repos"
 )
 
@@ -17,47 +18,37 @@ func NewTenderService(db database.TenderRepository) repos.TenderService {
 	}
 }
 
-// Получение списка предложений для тендера
-func (t *TenderServiceImpl) GetBidsForTender(ctx context.Context, tenderId repos.TenderId, params repos.GetBidsForTenderParams) error {
-	panic("impl me")
-}
-
-// Просмотр отзывов на прошлые предложения
-func (t *TenderServiceImpl) GetBidReviews(ctx context.Context, tenderId repos.TenderId, params repos.GetBidReviewsParams) error {
-	panic("impl me")
-}
-
 // Получение списка тендеров
-func (t *TenderServiceImpl) GetTenders(ctx context.Context, params repos.GetTendersParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) GetTenders(ctx context.Context, params repos.GetTendersParams) ([]*models.Tender, error) {
+	panic("not implemented") // TODO: Implement
 }
 
 // Получение тендеров пользователя
-func (t *TenderServiceImpl) GetUserTenders(ctx context.Context, params repos.GetUserTendersParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) GetUserTenders(ctx context.Context, params repos.GetUserTendersParams) ([]*models.Tender, error) {
+	panic("not implemented") // TODO: Implement
 }
 
 // Создание нового тендера
-func (t *TenderServiceImpl) CreateTender(ctx context.Context, params repos.CreateTenderParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) CreateTender(ctx context.Context, params repos.CreateTenderParams) (models.Tender, error) {
+	panic("not implemented") // TODO: Implement
 }
 
 // Редактирование тендера
-func (t *TenderServiceImpl) EditTender(ctx context.Context, tenderId repos.TenderId, username repos.Username, params repos.EditTenderParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) EditTender(ctx context.Context, tenderId repos.TenderId, username repos.Username, params repos.EditTenderParams) (models.Tender, error) {
+	panic("not implemented") // TODO: Implement
 }
 
 // Откат версии тендера
-func (t *TenderServiceImpl) RollbackTender(ctx context.Context, tenderId repos.TenderId, version int32, params repos.RollbackTenderParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) RollbackTender(ctx context.Context, tenderId repos.TenderId, version int32, params repos.RollbackTenderParams) (models.Tender, error) {
+	panic("not implemented") // TODO: Implement
 }
 
 // Получение текущего статуса тендера
-func (t *TenderServiceImpl) GetTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.GetTenderStatusParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) GetTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.GetTenderStatusParams) (repos.TenderStatus, error) {
+	panic("not implemented") // TODO: Implement
 }
 
 // Изменение статуса тендера
-func (t *TenderServiceImpl) UpdateTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.UpdateTenderStatusParams) error {
-	panic("impl me")
+func (b *TenderServiceImpl) UpdateTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.UpdateTenderStatusParams) (models.Tender, error) {
+	panic("not implemented") // TODO: Implement
 }
