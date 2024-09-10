2aeab604099a25dac5a72228150b0c75b6b23bb9
0x0FACED <https.genius@gmail.com>
2024-09-10 17:28:14 +0300

    feat: impl New func and Connect() method

--- internal/app/database/postgres/postgres.go
+++ internal/app/database/postgres/postgres.go
@@ -13,7 +13,24 @@ type Postgres struct {
 	cfg config.DatabaseConfig
 }
 
-func New() database.Database {
-	// ...
+func New(cfg config.DatabaseConfig) database.Database {
+	return &Postgres{
+		cfg: cfg,
+	}
+}
+
+// TODO: wrap errors
+func (p *Postgres) Connect() error {
+	db, err := sql.Open("postgres", p.cfg.ConnString)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	p.db = db
+
 	return nil
 }
