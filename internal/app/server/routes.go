bf309e6ac8a87a6f02a8dcf1d05326a257b806ff
0x0FACED <https.genius@gmail.com>
2024-09-11 13:29:21 +0300

    refactor: removed arg of RegisterRoutes, added /api to all the routes

--- internal/app/server/routes.go
+++ internal/app/server/routes.go
@@ -1,27 +1,23 @@
 package server
 
-import (
-	"github.com/labstack/echo/v4"
-)
-
 // RegisterHandlers регистрирует маршруты и связывает их с обработчиками
-func (s *server) RegisterHandlers(e *echo.Echo) {
-	s.r.GET("/bids/my", s.GetUserBids)
-	s.r.POST("/bids/new", s.CreateBid)
-	s.r.PATCH("/bids/:bidId/edit", s.EditBid)
-	s.r.PUT("/bids/:bidId/feedback", s.SubmitBidFeedback)
-	s.r.PUT("/bids/:bidId/rollback/:version", s.RollbackBid)
-	s.r.GET("/bids/:bidId/status", s.GetBidStatus)
-	s.r.PUT("/bids/:bidId/status", s.UpdateBidStatus)
-	s.r.PUT("/bids/:bidId/submit_decision", s.SubmitBidDecision)
-	s.r.GET("/bids/:tenderId/list", s.GetBidsForTender)
-	s.r.GET("/bids/:tenderId/reviews", s.GetBidReviews)
-	s.r.GET("/ping", s.CheckServer)
-	s.r.GET("/tenders", s.GetTenders)
-	s.r.GET("/tenders/my", s.GetUserTenders)
-	s.r.POST("/tenders/new", s.CreateTender)
-	s.r.PATCH("/tenders/:tenderId/edit", s.EditTender)
-	s.r.PUT("/tenders/:tenderId/rollback/:version", s.RollbackTender)
-	s.r.GET("/tenders/:tenderId/status", s.GetTenderStatus)
-	s.r.PUT("/tenders/:tenderId/status", s.UpdateTenderStatus)
+func (s *server) RegisterHandlers() {
+	s.r.GET("/api/bids/my", s.GetUserBids)
+	s.r.POST("/api/bids/new", s.CreateBid)
+	s.r.PATCH("/api/bids/:bidId/edit", s.EditBid)
+	s.r.PUT("/api/bids/:bidId/feedback", s.SubmitBidFeedback)
+	s.r.PUT("/api/bids/:bidId/rollback/:version", s.RollbackBid)
+	s.r.GET("/api/bids/:bidId/status", s.GetBidStatus)
+	s.r.PUT("/api/bids/:bidId/status", s.UpdateBidStatus)
+	s.r.PUT("/api/bids/:bidId/submit_decision", s.SubmitBidDecision)
+	s.r.GET("/api/bids/:tenderId/list", s.GetBidsForTender)
+	s.r.GET("/api/bids/:tenderId/reviews", s.GetBidReviews)
+	s.r.GET("/api/ping", s.CheckServer)
+	s.r.GET("/api/tenders", s.GetTenders)
+	s.r.GET("/api/tenders/my", s.GetUserTenders)
+	s.r.POST("/api/tenders/new", s.CreateTender)
+	s.r.PATCH("/api/tenders/:tenderId/edit", s.EditTender)
+	s.r.PUT("/api/tenders/:tenderId/rollback/:version", s.RollbackTender)
+	s.r.GET("/api/tenders/:tenderId/status", s.GetTenderStatus)
+	s.r.PUT("/api/tenders/:tenderId/status", s.UpdateTenderStatus)
 }
