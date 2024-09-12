6a48150b6f17423878cd958f51e226d97a4d35fa
0x0FACED <https.genius@gmail.com>
2024-09-12 17:46:17 +0300

    refactor: removed some comments, added error handling to some methods

--- internal/app/services_impl/bid_service_impl.go
+++ internal/app/services_impl/bid_service_impl.go
@@ -8,6 +8,10 @@ import (
 	"github.com/0x0FACED/tender-service/internal/app/domain/repos"
 )
 
+// TODO: после вызова методов обращения к БД добавить проверку ошибки
+// И вернуть ее
+// В методах бд надо возвращать разные ошибки, чтобы потом определять, какой http код вернуть юзеру
+
 const (
 	MAX_BID_NAME_SIZE        = 100
 	MAX_BID_DESCRIPTION_SIZE = 500
@@ -25,25 +29,12 @@ func NewBidService(db database.BidRepository) repos.BidService {
 }
 
 func (b *BidServiceImpl) CreateBid(ctx context.Context, params repos.CreateBidParams) (models.Bid, error) {
-	if err := ValidateCreateBid(params); err != nil {
+	if err := validateCreateBid(params); err != nil {
 		return models.Bid{}, err.Error()
 	}
 
-	// Если все гуд, то добавляем в базу
-	// Примечательно, что создавать bid можно как от имени организации, так и нет.
-	// Если от имени организации, то authorId будет айди организации
-	// И authorType будет 'Organization'
-
-	// дальнейшая логика:
-	/*
-	  1. Проверяем, есть ли тендер в бд (CheckIfTenderExistsByID)
-	  2. Проверяем наличие организации в бд (если не пустая строка)
-	  3. Проверяем наличие юзера в бд.
-	  4. Создаем бид.
-	*/
 	bid, err := b.db.CreateBid(ctx, params)
 	if err != nil {
-
 		return models.Bid{}, err
 	}
 	return *bid, nil
@@ -51,7 +42,7 @@ func (b *BidServiceImpl) CreateBid(ctx context.Context, params repos.CreateBidPa
 
 func (b *BidServiceImpl) GetUserBids(ctx context.Context, params repos.GetUserBidsParams) ([]*models.Bid, error) {
 
-	if err := ValidateGetUserBids(params); err != nil {
+	if err := validateGetUserBids(params); err != nil {
 		return nil, err.Error()
 	}
 	return b.db.GetUserBids(ctx, params)
@@ -59,7 +50,7 @@ func (b *BidServiceImpl) GetUserBids(ctx context.Context, params repos.GetUserBi
 
 func (b *BidServiceImpl) GetBidsForTender(ctx context.Context, tenderId repos.TenderId, params repos.GetBidsForTenderParams) ([]*models.Bid, error) {
 
-	if err := ValidateGetBidsForTender(params); err != nil {
+	if err := validateGetBidsForTender(params); err != nil {
 		return nil, err.Error()
 	}
 	return b.db.GetBidsForTender(ctx, tenderId, params)
@@ -67,7 +58,7 @@ func (b *BidServiceImpl) GetBidsForTender(ctx context.Context, tenderId repos.Te
 
 func (b *BidServiceImpl) GetBidStatus(ctx context.Context, bidId repos.BidId, params repos.GetBidStatusParams) (repos.BidStatus, error) {
 
-	if err := ValidateGetBidStatus(params); err != nil {
+	if err := validateGetBidStatus(params); err != nil {
 		return "", err.Error()
 	}
 	return b.db.GetBidStatus(ctx, bidId, params)
@@ -75,51 +66,69 @@ func (b *BidServiceImpl) GetBidStatus(ctx context.Context, bidId repos.BidId, pa
 
 func (b *BidServiceImpl) UpdateBidStatus(ctx context.Context, bidId repos.BidId, params repos.UpdateBidStatusParams) (models.Bid, error) {
 
-	if err := ValidateUpdateBidStatus(params); err != nil {
+	if err := validateUpdateBidStatus(params); err != nil {
 		return models.Bid{}, err.Error()
 	}
 	bid, err := b.db.UpdateBidStatus(ctx, bidId, params)
-	return *bid, err
+	if err != nil {
+		return models.Bid{}, err
+	}
+	return *bid, nil
 }
 
 func (b *BidServiceImpl) EditBid(ctx context.Context, bidId repos.BidId, username repos.Username, params repos.EditBidParams) (models.Bid, error) {
 
-	if err := ValidateEditBid(params); err != nil {
+	if err := validateEditBid(params); err != nil {
 		return models.Bid{}, err.Error()
 	}
 	bid, err := b.db.EditBid(ctx, bidId, username, params)
-	return *bid, err
+	if err != nil {
+		return models.Bid{}, err
+	}
+	return *bid, nil
 }
 
 func (b *BidServiceImpl) SubmitBidDecision(ctx context.Context, bidId repos.BidId, params repos.SubmitBidDecisionParams) (models.Bid, error) {
 
-	if err := ValidateSubmitBidDecision(params); err != nil {
+	if err := validateSubmitBidDecision(params); err != nil {
 		return models.Bid{}, err.Error()
 	}
 	bid, err := b.db.SubmitBidDecision(ctx, bidId, params)
-	return *bid, err
+	if err != nil {
+		return models.Bid{}, err
+	}
+	return *bid, nil
 }
 
 func (b *BidServiceImpl) SubmitBidFeedback(ctx context.Context, bidId repos.BidId, params repos.SubmitBidFeedbackParams) (models.Bid, error) {
-	if err := ValidateSubmitBidFeedback(params); err != nil {
+	if err := validateSubmitBidFeedback(params); err != nil {
 		return models.Bid{}, err.Error()
 	}
 	bid, err := b.db.SubmitBidFeedback(ctx, bidId, params)
-	return *bid, err
+	if err != nil {
+		return models.Bid{}, err
+	}
+	return *bid, nil
 }
 
 func (b *BidServiceImpl) RollbackBid(ctx context.Context, bidId repos.BidId, version int32, params repos.RollbackBidParams) (models.Bid, error) {
-	if err := ValidateRollbackBid(params); err != nil {
+	if err := validateRollbackBid(params); err != nil {
 		return models.Bid{}, err.Error()
 	}
 	bid, err := b.db.RollbackBid(ctx, bidId, version, params)
-	return *bid, err
+	if err != nil {
+		return models.Bid{}, err
+	}
+	return *bid, nil
 }
 
 func (b *BidServiceImpl) GetBidReviews(ctx context.Context, tenderId repos.TenderId, params repos.GetBidReviewsParams) ([]*models.BidReview, error) {
-	if err := ValidateGetBidReviews(params); err != nil {
+	if err := validateGetBidReviews(params); err != nil {
 		return nil, err.Error()
 	}
 	reviews, err := b.db.GetBidReviews(ctx, tenderId, params)
-	return reviews, err
+	if err != nil {
+		return nil, err
+	}
+	return reviews, nil
 }
