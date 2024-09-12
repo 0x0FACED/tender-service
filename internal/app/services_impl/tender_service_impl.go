72aa65fb6c45676b1441aa10737522ca9a00d791
0x0FACED <https.genius@gmail.com>
2024-09-12 17:47:44 +0300

    feat: added validations and error handling

--- internal/app/services_impl/tender_service_impl.go
+++ internal/app/services_impl/tender_service_impl.go
@@ -8,6 +8,10 @@ import (
 	"github.com/0x0FACED/tender-service/internal/app/domain/repos"
 )
 
+// TODO: после вызова методов обращения к БД добавить проверку ошибки
+// И вернуть ее
+// В методах бд надо возвращать разные ошибки, чтобы потом определять, какой http код вернуть юзеру
+
 type TenderServiceImpl struct {
 	db database.TenderRepository
 }
@@ -20,46 +24,72 @@ func NewTenderService(db database.TenderRepository) repos.TenderService {
 
 // Получение списка тендеров
 func (b *TenderServiceImpl) GetTenders(ctx context.Context, params repos.GetTendersParams) ([]*models.Tender, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateGetTenders(params); err != nil {
+		return nil, err.Error()
+	}
 	return b.db.GetTenders(ctx, params)
 }
 
 // Получение тендеров пользователя
 func (b *TenderServiceImpl) GetUserTenders(ctx context.Context, params repos.GetUserTendersParams) ([]*models.Tender, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateGetUserTenders(params); err != nil {
+		return nil, err.Error()
+	}
 	return b.db.GetUserTenders(ctx, params)
 }
 
 // Создание нового тендера
 func (b *TenderServiceImpl) CreateTender(ctx context.Context, params repos.CreateTenderParams) (models.Tender, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateCreateTender(params); err != nil {
+		return models.Tender{}, err.Error()
+	}
 	tender, err := b.db.CreateTender(ctx, params)
-	return *tender, err
+	if err != nil {
+		return models.Tender{}, err
+	}
+	return *tender, nil
 }
 
 // Редактирование тендера
 func (b *TenderServiceImpl) EditTender(ctx context.Context, tenderId repos.TenderId, username repos.Username, params repos.EditTenderParams) (models.Tender, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateEditTender(params); err != nil {
+		return models.Tender{}, err.Error()
+	}
 	tender, err := b.db.EditTender(ctx, tenderId, username, params)
-	return *tender, err
+	if err != nil {
+		return models.Tender{}, err
+	}
+	return *tender, nil
 }
 
 // Откат версии тендера
 func (b *TenderServiceImpl) RollbackTender(ctx context.Context, tenderId repos.TenderId, version int32, params repos.RollbackTenderParams) (models.Tender, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateRollbackTender(params); err != nil {
+		return models.Tender{}, err.Error()
+	}
 	tender, err := b.db.RollbackTender(ctx, tenderId, version, params)
-	return *tender, err
+	if err != nil {
+		return models.Tender{}, err
+	}
+	return *tender, nil
 }
 
 // Получение текущего статуса тендера
 func (b *TenderServiceImpl) GetTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.GetTenderStatusParams) (repos.TenderStatus, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateGetTenderStatus(params); err != nil {
+		return "", err.Error()
+	}
 	return b.db.GetTenderStatus(ctx, tenderId, params)
 }
 
 // Изменение статуса тендера
 func (b *TenderServiceImpl) UpdateTenderStatus(ctx context.Context, tenderId repos.TenderId, params repos.UpdateTenderStatusParams) (models.Tender, error) {
-	// TODO: Добавить валидацию!!!
+	if err := validateUpdateTenderStatus(params); err != nil {
+		return models.Tender{}, err.Error()
+	}
 	tender, err := b.db.UpdateTenderStatus(ctx, tenderId, params)
-	return *tender, err
+	if err != nil {
+		return models.Tender{}, err
+	}
+	return *tender, nil
 }
